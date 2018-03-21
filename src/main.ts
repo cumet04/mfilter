e1fe518685208c32d691c68ec0cb08a3ee623c2b
medalhkr <medalhkr@gmail.com>
2018-03-21 13:21:27 +0900

    clean tsconfig options

--- src/main.ts
+++ src/main.ts
@@ -5,15 +5,18 @@ function main() {
     mfilter.execute([
         (mail: mfilter.Mail) => {
             Logger.log("flow1.action")
+            Logger.log(`title: ${mail.getSubject()}`)
             Logger.log(mail.getDate())
             return true
         },
         (mail: mfilter.Mail) => {
             Logger.log("flow2.action")
+            Logger.log(`title: ${mail.getSubject()}`)
             return false
         },
         (mail: mfilter.Mail) => {
             Logger.log("flow3.action")
+            Logger.log(`title: ${mail.getSubject()}`)
             return true
         },
     ])
