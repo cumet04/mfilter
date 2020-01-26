4e725852858320e14dabc136398d5b29e8310e8b
cumet04 <medalhkr@gmail.com>
2020-01-26 09:02:39 +0000

    add sample

--- gas/actions.ts
+++ gas/actions.ts
@@ -2,6 +2,8 @@ type Message = GoogleAppsScript.Gmail.GmailMessage;
 type Thread = GoogleAppsScript.Gmail.GmailThread;
 
 export module Actions {
+  export type Action = (t: Message | Thread) => void;
+
   let dry_run = false;
   export function setDryRun(v: boolean) {
     dry_run = v;
--- gas/main.ts
+++ gas/main.ts
@@ -3,29 +3,36 @@ type Message = GoogleAppsScript.Gmail.GmailMessage;
 type Thread = GoogleAppsScript.Gmail.GmailThread;
 
 import { Actions } from "./actions";
+type Filter = (t: Thread) => boolean;
 
 function main() {
   Actions.setDryRun(true);
 
   const query = "in:inbox -in:chats";
-  const filters = [
-    (thread: Thread) => {
-      console.log("filter1");
-      return true;
-    },
-    (thread: Thread) => {
-      console.log("filter2");
-      return false;
-    },
-    (thread: Thread) => {
-      console.log("filter3");
-      return true;
-    }
-  ];
 
+  execute(
+    query,
+    [
+      (thread: Thread) => {
+        const from = thread.getMessages()[0].getFrom();
+        return from.includes("notifications@github.com");
+      }
+    ],
+    (target: Thread) => {
+      Actions.markRead(target);
+    }
+  );
+}
+
+function execute(
+  query: string,
+  filters: Array<Filter>,
+  action: Actions.Action
+) {
   GmailApp.search(query).forEach(thread => {
     for (let filter of filters) {
       if (!filter(thread)) return;
+      action(thread);
     }
   });
 }
